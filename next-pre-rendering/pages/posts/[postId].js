a363cd27056dad86f15092e8a74ec830ef71eab0
Jayden <cjy0029@naver.com>
2022-07-30 01:59:15 +0900

    fallback true finish

--- next-pre-rendering/pages/posts/[postId].js
+++ next-pre-rendering/pages/posts/[postId].js
@@ -1,4 +1,12 @@
+import { useRouter } from 'next/router';
+
 const Post = ({ post }) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
     <>
       <h2>
@@ -10,6 +18,17 @@ const Post = ({ post }) => {
 };
 
 export async function getStaticPaths() {
+  // const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  // const data = await response.json();
+
+  // const paths = data.map((post) => {
+  //   return {
+  //     params: {
+  //       postId: `${post.id}`,
+  //     },
+  //   };
+  // });
+
   return {
     paths: [
       {
@@ -22,7 +41,8 @@ export async function getStaticPaths() {
         params: { postId: '3' },
       },
     ],
-    fallback: false,
+    // paths,
+    fallback: true,
   };
 }
 
@@ -32,6 +52,14 @@ export async function getStaticProps(context) {
     `https://jsonplaceholder.typicode.com/posts/${params.postId}`,
   );
   const data = await response.json();
+
+  if (!data.id) {
+    return {
+      notFound: true,
+    };
+  }
+
+  console.log(`Generating page for /posts/${params.postId}`);
 
   return {
     props: {
