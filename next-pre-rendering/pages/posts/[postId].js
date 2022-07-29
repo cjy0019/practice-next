11113f01bd852e4eb4b19ea1f1c8217d3fc3ab6e
Jayden <cjy0029@naver.com>
2022-07-30 01:18:55 +0900

    getStaticPath modify

--- next-pre-rendering/pages/posts/[postId].js
+++ next-pre-rendering/pages/posts/[postId].js
@@ -9,6 +9,23 @@ const Post = ({ post }) => {
   );
 };
 
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: { postId: '1' },
+      },
+      {
+        params: { postId: '2' },
+      },
+      {
+        params: { postId: '3' },
+      },
+    ],
+    fallback: false,
+  };
+}
+
 export async function getStaticProps(context) {
   const { params } = context;
   const response = await fetch(
